7155787c6f1a5868de7eb4747f85b55f2e708453
dlent1 <dave_meade@yahoo.com>
2023-07-20 19:34:37 -0500

    first commit

---
+++ DatePickerBoldItalicNotWorking/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DatePickerBoldItalicNotWorking;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ DatePickerBoldItalicNotWorking/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DatePickerBoldItalicNotWorking;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
